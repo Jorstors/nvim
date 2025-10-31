c49e5f2b2595ebe5acf2b1ad89ff45fadbd7cba9
Justus <justus1274@gmail.com>
2025-10-30 22:46:21 -0700

    Added scroll functionality to opencode plugin

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -19,19 +19,24 @@ return {
     vim.keymap.set({ "n", "x" }, "<C-a>", function()
       require("opencode").ask("@this: ", { submit = true })
     end, { desc = "Ask opencode" })
+
     vim.keymap.set({ "n", "x" }, "<C-x>", function()
       require("opencode").select()
     end, { desc = "Execute opencode action…" })
+
     vim.keymap.set({ "n", "x" }, "ga", function()
       require("opencode").prompt("@this")
     end, { desc = "Add to opencode" })
+
     vim.keymap.set("n", "<leader>o", function()
       require("opencode").toggle()
     end, { desc = "Toggle opencode" })
-    vim.keymap.set("n", "<S-C-u>", function()
+
+    vim.keymap.set("n", "<C-u>", function()
       require("opencode").command("messages_half_page_up")
     end, { desc = "opencode half page up" })
-    vim.keymap.set("n", "<S-C-d>", function()
+
+    vim.keymap.set("n", "<C-d>", function()
       require("opencode").command("messages_half_page_down")
     end, { desc = "opencode half page down" })
     -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o".
